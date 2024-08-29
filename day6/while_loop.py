2e12914dca79715c14f82b4d5a3347ca78ac23a3
War-Oxi <xowl5460@naver.com>
2024-08-29 23:06:14 +0900

    you're doing great

---
+++ day6/while_loop.py
@@ -0,0 +1,12 @@
+# 조건이 참인 블록을 반복으로 실행하는 반복문 => while
+
+# 예시 - num1부터 num2까지 더하기
+def sum(num1, num2):
+    result = 0
+    while num1 != num2 + 1:
+        result += num1
+        num1 += 1
+        
+    return result
+
+print(sum(1, 10))
