d69586636ed56ba665ba4b2cb58b57083ac56ab8
KKamji <xowl5460@naver.com>
2024-09-28 23:12:57 +0900

    you're doing great

---
+++ day-025/README.md
@@ -0,0 +1,4 @@
+# Goals: CSV 파일 사용 방법 & Pandas 라이브러리를 이용한 데이터 분석
+
+pandas는 DataFrame과 Series로 나뉜다. DataFrame => 2차원 series => 1차원
+
---
+++ day-025/creat_dataframe.py
@@ -0,0 +1,13 @@
+import pandas
+import os
+
+PATH = os.path.join(os.path.dirname(__file__), "new_data.csv")
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+# print(data)
+data.to_csv(PATH)
