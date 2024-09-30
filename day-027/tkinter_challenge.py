0a11006f779a102f1990a4b02f61ed2a6c0c135f
KKamji <xowl5460@naver.com>
2024-10-01 07:56:48 +0900

    you're doing great

--- day-027/tkinter_challenge.py
+++ day-027/tkinter_challenge.py
@@ -9,9 +9,11 @@ label.config(text="My name is Kim Tae-Ji", font=("Arial", 24, "bold"))
 label.grid(column=0, row=0)
 
 button = tkinter.Button()
+button.config(text="My Button", font=("Arial", 24, "bold"))
 button.grid(column=1, row=1)
 
 new_button = tkinter.Button()
+new_button.config(text="New Button", font=("Arial", 24, "bold"))
 button.grid(column=2, row=0)
 
 entry = tkinter.Entry()
