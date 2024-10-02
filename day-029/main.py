3905ba7e01e3f804def118b01c836eb6514c4315
KKamji <xowl5460@naver.com>
2024-10-02 14:15:34 +0900

    you're doing great

--- day-029/main.py
+++ day-029/main.py
@@ -10,7 +10,7 @@ IMAGE_PATH = os.path.join(os.path.dirname(__file__), "logo.png")
 
 window = tkinter.Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 logo_image = tkinter.PhotoImage(file=IMAGE_PATH)
 image_width = logo_image.width()
@@ -22,17 +22,17 @@ canvas.grid(column=1, row=0)
 
 # Website
 website_label = tkinter.Label(text="Website:", font="Courier")
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=1, sticky="W")
 
-website_entry = tkinter.Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = tkinter.Entry(width=55)
+website_entry.grid(column=1, row=1, columnspan=2, sticky="W")
 
 # Email/Username
 email_and_username_label = tkinter.Label(text="Email/Username:", font="Courier")
-email_and_username_label.grid(column=0, row=2)
+email_and_username_label.grid(column=0, row=2, sticky="W")
 
-email_and_username_entry = tkinter.Entry(width=35)
-email_and_username_entry.grid(column=1, row=2, columnspan=2)
+email_and_username_entry = tkinter.Entry(width=55)
+email_and_username_entry.grid(column=1, row=2, columnspan=2, sticky="W")
 
 # Password
 
@@ -40,20 +40,20 @@ def generate_password():
     pass
 
 password_label = tkinter.Label(text="Password:", font="Courier")
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=3, sticky="W")
 
-password_entry = tkinter.Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry = tkinter.Entry(width=30)
+password_entry.grid(column=1, row=3, sticky="W")
 
 password_button = tkinter.Button(text="Generate Password", command=generate_password)
-password_button.grid(column=2, row=3)
+password_button.grid(column=2, row=3, sticky="W")
 
 # Add
 def add_password():
     pass
 
 add_button = tkinter.Button(text="Add", command=add_password, width=36)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(column=1, row=4, columnspan=2, sticky="W")
 
 
 
